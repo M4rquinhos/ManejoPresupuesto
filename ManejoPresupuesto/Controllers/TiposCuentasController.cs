eea461232f1a8bcd294ca584c1c9daf0c6f05de7
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2023-08-05 16:08:09 -0600

    Creando el formulario tipo cuentas

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ManejoPresupuesto.Controllers
 {
     public class TiposCuentasController : Controller
     {
         public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
         {
             return View();
         }
