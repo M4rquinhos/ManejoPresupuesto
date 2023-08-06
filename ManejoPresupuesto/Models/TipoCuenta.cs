b0886f6d9a199204a25b0a9893027b2963920b0a
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2023-08-06 01:48:36 -0600

    Visualizando los erroes de validacion

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -6,7 +6,8 @@ namespace ManejoPresupuesto.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe de estar entre {2} y {1}")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
