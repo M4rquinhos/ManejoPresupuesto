a93382330a7aec3c0124eb6443a4b7802e5a6c2d
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2023-11-13 23:16:50 -0600

    Agregando fomrulario de cuentas

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,33 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas,
+            IServicioUsuarios servicioUsuarios)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+
+            var usuarioId = servicioUsuarios.ObtenerUsuario();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            var modelo = new CuentaCreacionViewModel()
+            {
+                TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()))
+            };
+            return View(modelo); 
+        }
+    }
+}
